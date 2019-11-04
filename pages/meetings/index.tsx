6afd209016aa4a22b3d6fe5e74c0c772f097c4e1
hasparus <hasparus@gmail.com>
2019-11-04 22:37:53 +0100

    Add more stub code to /meetings

--- pages/meetings/index.tsx
+++ pages/meetings/index.tsx
@@ -11,4 +11,8 @@ const MeetingsPage = ({ meetings }: MeetingsPageProps) => (
   <Fragment>{meetings.map(m => JSON.stringify(m))}</Fragment>
 );
 
+MeetingsPage.getInitialProps = async () => {
+  return { meetings: [] };
+};
+
 export default MeetingsPage;
