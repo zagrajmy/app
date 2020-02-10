3c9a1f361d26182643ef85cdc03f63d00b60f114
hasparus <hasparus@gmail.com>
2020-02-10 16:37:56 +0100

    WIP

--- src/ui/theme.tsx
+++ src/ui/theme.tsx
@@ -21,6 +21,7 @@ export const theme: themeUi.Theme = {
     "rounded-sm": ".125rem",
     rounded: ".25rem",
     "rounded-lg": ".5rem",
+    round: 999999,
   },
   shadows: {
     sm: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
