4ad09606c86ca761254caec4c66368123ffec9d4
hasparus <hasparus@gmail.com>
2020-03-12 01:14:48 +0100

    Add a script to create test meetings

--- data/hasura.ts
+++ data/hasura.ts
@@ -5,6 +5,10 @@ import { Chain } from "./graphql-zeus";
 const _global = typeof window !== "undefined" ? window : globalThis;
 Object.assign(_global, { fetch: _global.fetch || fetch });
 
-const { HASURA_URL } = process.env;
+const { HASURA_URL, HASURA_ADMIN_SECRET } = process.env;
 
-export const hasura = Chain(`${HASURA_URL}/v1/graphql`);
+export const hasura = Chain(`${HASURA_URL}/v1/graphql`, {
+  headers: {
+    "X-Hasura-Admin-Secret": HASURA_ADMIN_SECRET!,
+  },
+});
