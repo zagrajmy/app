c377ff5816d1b535e80fd28f260d700e406d4655
hasparus <hasparus@gmail.com>
2020-03-22 00:03:44 +0100

    Get rid of next-cookies

--- src/declarations/missing.d.ts
+++ src/declarations/missing.d.ts
@@ -1,11 +1,3 @@
-declare module "next-cookies" {
-  import { NextPageContext } from "next";
-
-  const nextCookies: (ctx: NextPageContext) => object;
-  // eslint-disable-next-line import/no-default-export
-  export default nextCookies;
-}
-
 declare module "graphqurl";
 
 declare module "@theme-ui/color-modes";
