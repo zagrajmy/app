b435a3306175e9dcc45ae65289c01d02eb5a6649
hasparus <hasparus@gmail.com>
2020-03-22 20:26:23 +0100

    Update path deploy.sh

--- packages/app/data/throwaway/deploy.sh
+++ packages/app/data/throwaway/deploy.sh
@@ -1,3 +1,3 @@
-git subtree split --prefix data/throwaway -b temporary
+git subtree split --prefix packages/app/data/throwaway -b temporary
 git push -f heroku temporary:master
 git branch -D temporary 
