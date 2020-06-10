1a6848ddf1071cc4da33f51343c4346139396977
hasparus <hasparus@gmail.com>
2020-06-10 08:33:38 +0200

    Add FormLabel utility type and reexport Label

---
+++ packages/app/src/ui/Label.tsx
@@ -0,0 +1,8 @@
+import { LabelProps } from "theme-ui";
+
+export { Label } from "theme-ui";
+
+export interface FormLabelProps<Name> extends Omit<LabelProps, "htmlFor"> { htmlFor: Name }
+export type FormLabel<Name> = (props: FormLabelProps<Name>) => JSX.Element;
+
+export type { LabelProps };
