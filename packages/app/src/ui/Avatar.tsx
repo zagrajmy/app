ddff734809dd0dfe836ec2c3ca316540b687fdc0
hasparus <hasparus@gmail.com>
2020-06-26 19:04:58 +0200

    Render fallback in Avatars

--- packages/app/src/ui/Avatar.tsx
+++ packages/app/src/ui/Avatar.tsx
@@ -1,16 +1,44 @@
-import { forwardRef, Ref } from "react";
+import { forwardRef, Ref, useEffect, useRef, useState } from "react";
 import {
   Avatar as ThemeUIAvatar,
   AvatarProps as ThemeUIAvatarProps,
   Box,
 } from "theme-ui";
 
-export interface AvatarProps extends Omit<ThemeUIAvatarProps, "ref"> {}
+import { noop } from "../lib";
+import { Center } from "./Center";
+
+const PlaceholderText = ({ children }: { children?: string }) => (
+  <Center sx={{ position: "absolute", size: "100%" }}>{children}</Center>
+);
+
+export interface AvatarProps extends Omit<ThemeUIAvatarProps, "ref"> {
+  placeholder?: string;
+}
 
 export const Avatar = forwardRef(function Avatar(
-  { className, ...rest }: AvatarProps,
+  { className, src, placeholder, ...rest }: AvatarProps,
   ref: Ref<HTMLElement>
 ) {
+  const [state, setState] = useState<"ok" | "error">("ok");
+  const avatarRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const img = avatarRef.current;
+    if (img) {
+      const onError = () => {
+        setState("error");
+      };
+      img.addEventListener("error", onError);
+
+      return () => {
+        img.removeEventListener("error", onError);
+      };
+    }
+
+    return noop;
+  }, []);
+
   return (
     <Box
       sx={{
@@ -18,6 +46,8 @@ export const Avatar = forwardRef(function Avatar(
         height: 32,
         borderRadius: "round",
         overflow: "hidden",
+        position: "relative",
+        display: "inline-block",
       }}
       className={className}
       ref={ref as Ref<HTMLDivElement>}
@@ -29,8 +59,17 @@ export const Avatar = forwardRef(function Avatar(
         importance="low"
         width={32}
         height={32}
+        src={src}
+        ref={avatarRef}
+        sx={{
+          position: "absolute",
+          display: state === "ok" ? "block" : "none",
+        }}
         {...rest}
       />
+      {state === "error" && (
+        <PlaceholderText>{placeholder || rest.alt?.[0]}</PlaceholderText>
+      )}
       <Box
         sx={{
           bg: "gray.3",
