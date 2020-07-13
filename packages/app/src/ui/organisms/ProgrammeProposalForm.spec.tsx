c6a07a5b5fe2d055c48523d0e9f1ac55e0c9d25a
hasparus <hasparus@gmail.com>
2020-07-13 12:23:46 +0200

    Update workspace dependencies

--- packages/app/src/ui/organisms/ProgrammeProposalForm.spec.tsx
+++ packages/app/src/ui/organisms/ProgrammeProposalForm.spec.tsx
@@ -1,1 +1,9 @@
-export {};
+import { render } from "@testing-library/react";
+
+import { ProgrammeProposalForm } from "./ProgrammeProposalForm";
+
+describe(ProgrammeProposalForm.name, () => {
+  it("does something", () => {
+    expect(true);
+  });
+});
