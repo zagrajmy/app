e7acc0810648c520b15f3290fd2c2dfa2ff7e282
hasparus <hasparus@gmail.com>
2020-07-13 12:23:46 +0200

    Update workspace dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testPathIgnorePatterns: ["docs/.cache", "app/test/e2e"],
+  globals: {
+    "ts-jest": {
+      babelConfig: "packages/app/.babelrc",
+    },
+  },
 };
