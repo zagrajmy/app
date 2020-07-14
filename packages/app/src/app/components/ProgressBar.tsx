8917879016167799b97614431835c5c8b3956a46
hasparus <hasparus@gmail.com>
2020-07-14 10:03:19 +0200

    Move ProgressBar to separate file and import it dynamically

---
+++ packages/app/src/app/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+import NextJsProgressBar from "nextjs-progressbar";
+import React from "react";
+
+import { useTheme } from "../../ui/theme";
+
+const progressBarOptions = { showSpinner: false };
+
+export const ProgressBar = () => {
+  const {
+    theme: { colors },
+  } = useTheme();
+  return (
+    <NextJsProgressBar color={colors.primary} options={progressBarOptions} />
+  );
+};
+
+// for dynamic import
+// eslint-disable-next-line import/no-default-export
+export default ProgressBar;
