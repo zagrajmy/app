540d1effcd745b17a7c5dd4de6403be0178b4b72
hasparus <hasparus@gmail.com>
2020-07-20 02:34:49 +0200

    Add favicon

--- packages/app/src/app/components/CommonHead.tsx
+++ packages/app/src/app/components/CommonHead.tsx
@@ -1,19 +1,21 @@
 import Head from "next/head";
 import * as React from "react";
-import { useTranslation } from "react-i18next";
 
-import { useAppState } from "../store";
+import { useTheme } from "../../ui/theme";
+import { useAppName } from "../store/useAppName";
 
 export function CommonHead() {
-  const { t } = useTranslation();
-  const { sphere } = useAppState();
-
-  const appName = t("page-title") || sphere.name || "zagrajmy";
+  const appName = useAppName();
+  const { theme } = useTheme();
 
   return (
     <Head>
       <title>{appName}</title>
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content={theme.colors.primary} />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      {/* <link rel="mask-icon" href="mask-icon.svg" color="#000000" /> */}
+      {/* <link rel="apple-touch-icon" href="apple-touch-icon.png" /> */}
+      {/* <link rel="manifest" href="manifest.json" /> */}
     </Head>
   );
 }
