02765786fb8d1de27958bb8be8843d32c04e57b5
hasparus <hasparus@gmail.com>
2020-07-21 09:28:58 +0200

    Fix sphere url detection

--- packages/app/src/lib/getUrl.ts
+++ packages/app/src/lib/getUrl.ts
@@ -3,14 +3,14 @@ import { IncomingMessage } from "http";
 export const getUrl = ({
   headers: { referer, host } = {},
 }: Pick<IncomingMessage, "headers">) => {
-  if (referer && referer !== "/") {
-    return referer;
+  if (typeof window !== "undefined") {
+    return window.location.origin;
   }
   if (host) {
     return `http${host.includes("localhost") ? "" : "s"}://${host}`;
   }
-  if (typeof window !== "undefined") {
-    return window.location.origin;
+  if (referer && referer !== "/") {
+    return referer;
   }
   throw new Error("cant get URL");
 };
