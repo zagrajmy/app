32d80d235fc48497505997dbd0caa204adb0875b
hasparus <hasparus@gmail.com>
2020-08-09 13:15:22 +0200

    Add UI for intermediate state between proposal_end and start_publication

--- packages/app/src/ui/mdx.tsx
+++ packages/app/src/ui/mdx.tsx
@@ -2,10 +2,12 @@ import { htmdx } from "htmdx";
 import { jsx, Styled } from "theme-ui";
 
 import { Link } from "../ui/Link";
+import { Text } from "../ui/Text";
 
 const components = {
   ...Styled,
   Link,
+  Text,
 };
 
 export const mdx = (s: string) => htmdx(s, jsx, { components });
