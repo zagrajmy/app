6d10c57b90c84f69ab017443c125ff26cef25945
hasparus <hasparus@gmail.com>
2020-10-17 22:33:41 +0200

    Add typed postParticipant

--- packages/app/src/app/api-helpers/zagrajmyRestApi/participants.ts
+++ packages/app/src/app/api-helpers/zagrajmyRestApi/participants.ts
@@ -1,11 +1,15 @@
-import { zagrajmyRestApi } from "./index";
+import { ZagrajmyRestApi, zagrajmyRestApi } from "./index";
 
 export const deleteParticipant = (meetingId: string) =>
   zagrajmyRestApi(`notice_board/meeting/${meetingId}/participants`, {
     method: "DELETE",
   });
 
-export const postParticipant = (meetingId: string) =>
-  zagrajmyRestApi(`notice_board/meeting/${meetingId}/add_participant`, {
-    // body: TODO
+export const postParticipant = (
+  meetingId: ZagrajmyRestApi.AddParticipantMeeting.PathParameters["id"],
+  json: ZagrajmyRestApi.AddParticipantMeeting.RequestBody
+) => {
+  return zagrajmyRestApi(`notice_board/meeting/${meetingId}/add_participant`, {
+    json: { ...json },
   });
+};
