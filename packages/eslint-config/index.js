58b9734b85699e6f44645fe36bac9bcb1ced6d5c
hasparus <hasparus@gmail.com>
2020-10-26 08:41:00 +0100

    Update ESLint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,33 +1,16 @@
-module.exports = {
-  extends: "chop-chop",
-  plugins: ["simple-import-sort"],
-  rules: {
-    "sonarjs/no-duplicate-string": "off",
-    "jsx-a11y/anchor-has-content": "off",
-    "jsx-a11y/anchor-is-valid": "off",
-    "no-restricted-syntax": "off",
-    "no-nested-ternary": "off",
+const {
+  makeBasePreset,
+} = require("@hasparus/eslint-config/src/makeBasePreset");
 
-    "react/prop-types": "off",
-    "react/no-array-index-key": "off",
-    // false positives on `user?: never;`
-    "react/no-unused-prop-types": "off",
+const config = makeBasePreset({
+  ownPackageScope: "zagrajmy",
+  useRulesRequiringTypechecking: process.env.CI === "true",
+});
 
-    "no-undef-init": "off",
-    // TypeScript checks this
-    "no-undef": "off",
-
-    "sort-imports": "off",
-    "import/order": "off",
-    "simple-import-sort/sort": "warn",
-    "no-else-return": "off",
-    "import/no-default-export": "off",
-
-    "react/jsx-pascal-case": "off",
-
-    "prefer-destructuring": ["warn", { object: true, array: false }],
-
-    "no-void": "off",
-    camelcase: "off",
-  },
+config.rules = {
+  ...config.rules,
+  // TODO: Configuration for rule "@typescript-eslint/ban-types" is invalid: 	Value false should be null. 	Value false should be string. 	Value false should be object. 	Value false should match exactly one schema in oneOf.
+  "@typescript-eslint/ban-types": "off",
 };
+
+module.exports = config;
